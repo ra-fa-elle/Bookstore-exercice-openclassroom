9d76a947296aeb40287d1806812f2a19e982406e
ra-fa-elle <raphaelle.clerici@gmail.com>
2021-02-23 20:42:59 +0100

    Tuto complete

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -12,6 +12,9 @@ import { Book } from 'src/app/models/book.model';
 export class BookFormComponent implements OnInit {
 
   bookForm: FormGroup;
+  fileIsUploading = false;
+  fileUrl: string;
+  fileUploaded = false;
 
   constructor(private formBuilder: FormBuilder, 
               private bookservices: BooksService, 
@@ -37,7 +40,29 @@ export class BookFormComponent implements OnInit {
     const author = this.bookForm.get('author').value;
     const newBook = new Book(title, author);
 
+    // vérif s'il y a ou non une photo
+    if(this.fileUrl && this.fileUrl !== '') {
+      newBook.photo = this.fileUrl;
+    }
     this.bookservices.createNewBook(newBook);
     this.router.navigate(['/books']);
+  }
+
+  onUploadFile(file: File)
+  {
+    this.fileIsUploading = true;
+    this.bookservices.uploadFile(file).then(
+      (url: string) =>
+      {
+        this.fileUrl = url;
+        this.fileIsUploading = false;
+        this.fileUploaded = true;
+      }
+    )
+  }
+
+  detectFiles(event)
+  {
+    this.onUploadFile(event.target.files[0]);
   }
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -60,6 +60,19 @@ export class BooksService {
 
   removeBook(book: Book)
   {
+    if(book.photo){
+      const storageRef = firebase.storage().refFromURL(book.photo);
+      storageRef.delete().then(
+        () => {
+          console.log('photo supprimée')
+        }
+      ).catch(
+        (error) => {
+          console.log('Fichier non touvé: ' + error);
+        }
+      )
+    }
+    
     const bookIndexToRemove = this.books.findIndex(
       (bookEl) =>
       {
@@ -71,5 +84,32 @@ export class BooksService {
     this.books.splice(bookIndexToRemove, 1);
     this.saveBooks();
     this.emitBooks();
+  }
+
+  // Méthode pour upload photo
+  uploadFile(file: File)
+  {
+    return new Promise(
+      (resolve, reject) =>
+      {
+        const almostUniqueFileName = Date.now().toString();
+        const upload = firebase.storage().ref()
+                               .child('images/' + almostUniqueFileName + file.name)
+                               .put(file);
+        // on réagit à chaque changement d'état du DL
+        upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
+          () => {
+            console.log('chargement...');
+          },
+          (error) => {
+            console.log('erreur de chargement' + error);
+            reject();
+          },
+          () => {
+            resolve(upload.snapshot.downloadURL);
+          }
+        )
+      }
+    )
   }
 }
